85fe61c79db76fa73cb1ad80ed75bef83b6415a9
Jared Kohrt <jkohrt7@gmail.com>
2022-03-03 20:28:06 -0500

    first draft of PC menu layout

--- src/App.js
+++ src/App.js
@@ -13,22 +13,56 @@ import './App.css'
 function App() {
   
   //Controls the camera position.
-  const [zoom, setZoom] = useState(10);
+  const [zoom, setZoom] = useState(1);
   function moveCamera() {
     let newZoom = zoom + 50;
     setZoom(newZoom);
   }
 
+  //Get a list of asteroids passing in the next week from the NASA NEO api
+  const [allAsteroidData, setAsteroidData] = useState(false);
+  useEffect(() => {
+      let today = new Date();
+
+      let dd = String(today.getDate()).padStart(2, '0');
+      let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+      let yyyy = today.getFullYear();
+
+      today = yyyy + '-' + mm + '-' + dd;
+
+      fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${today}&end_data=${today}&api_key=hMvAKv2iX56ehyLToioQbO6OMPqUmVH9HTDQEJhe`)
+          .then(res => res.json())
+          .then(data => {
+            //Change from *inhales* object of arrays of objects to just array of objects
+            let dataArr = [];
+            
+            for(let dateProperty in data.near_earth_objects) {
+                data.near_earth_objects[dateProperty].map((item) => {
+                    dataArr.push(item)
+                    return;
+                }) 
+            }
+            setAsteroidData(dataArr)
+
+          })
+          .catch((error) => {
+              console.error('Error: ' + error)
+      })
+  }, [])
+
+  //Keep track of the selected asteroid
+  const [currAsteroid, setCurrAsteroid] = useState(false);
+
   return (
     <div id = "main-container">
       <BasicScene>
         {/* <Suspense fallback = {null}>
-          <Asteroid zoom = {zoom} position = {[zoom/2,0,0]} scale = {zoom/8}/>
-          <Astronaut scale = {[0.1,0.1,0.1]} position = {[-2,0,0]}/>
+          <Asteroid zoom = {1} position = {[.5,0,0]} scale = {0.1} data = {allAsteroidData}/>
+          <Astronaut scale = {[0.01,0.01,0.01]} position = {[0,0,0]}/>
           <SpaceBG position = {[0,0,-50]}/>
         </Suspense> */}
       </BasicScene>
-      <AsteroidList/>
+      <AsteroidList data = {allAsteroidData} currAsteroid = {currAsteroid} asteroidSetter = {setCurrAsteroid} />
     </div>
   );
 }
--- src/components/AsteroidList.js
+++ src/components/AsteroidList.js
@@ -1,42 +1,50 @@
 import './AsteroidList.css';
 import { useEffect } from 'react';
 
-function AsteroidList() {
-    
-    useEffect(() => {
-        
-    })
+function AsteroidList(props) {
 
-    const asteroids = [{name: "namehere", size: "sizeHere", distance: "distanceHere"},
-    {name: "namehere", size: "sizeHere", distance: "distanceHere"}];
-    const listItems = asteroids.map((item, index) => {
-        return(
-            <tr key = {item.name} className = {index % 2 ? "light-row" : "dark-row"} >
-                <td>{item.name}</td>
-                <td>{item.size}</td>
-                <td>{item.distance}</td>
-            </tr>
-        )
-    })
+    function renderListItems(data) {
+        let tableRowHTML = data.map((item, index) => {
+            return(
+                <tr 
+                key = {item.id} 
+                className = {(index % 2 ? "light-row" : "dark-row") + " " + (props.currAsteroid.id === item.id ? "selected" : "")} 
+                onClick = {() => setAsteroid(item.id)}
+                >
+                    <td>{item.name}</td>
+                    <td>{Math.round(item.estimated_diameter.feet.estimated_diameter_min) + " Ft"}</td>
+                    <td>{item.close_approach_data[0].close_approach_date}</td>
+                </tr>
+            )
+        })
 
+        return tableRowHTML;
+    }
 
+    //Changes the selected Asteroid
+    function setAsteroid(id) {
+        let asteroid = props.data.find(element => element.id === id)
+        props.asteroidSetter(asteroid)
+    }
 
     return (
-        <div className = "for-margins" >
+        <div className = "outermost-container" >
             <div className = "menu-container">
                 <div className = "header">
                     Asteroids or something
                 </div>
                 <div className = "list-container">
                     <table>
-                        <tr className = "table-header">
-                            <th>Name</th>
-                            <th>Size</th>
-                            <th>Distance</th>
-                        </tr>
-
-                        {listItems}
-
+                        <thead>
+                            <tr className = "table-header">
+                                <th>Name</th>
+                                <th>Size (min)</th>
+                                <th>Passes on</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {props.data ? renderListItems(props.data) : null}
+                        </tbody>
                     </table>
                 </div>
             </div>
