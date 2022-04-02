428fcee33625712f38497ec28229d4273acca61c
Jared Kohrt <jkohrt7@gmail.com>
2022-04-02 18:21:37 -0400

    refactored table w divs

--- src/components/AsteroidList.js
+++ src/components/AsteroidList.js
@@ -7,15 +7,15 @@ function AsteroidList(props) {
     function renderListItems(data) {
         let tableRowHTML = data.map((item, index) => {
             return(
-                <tr 
+                <div 
                 key = {item.id} 
                 className = {(index % 2 ? "light-row" : "dark-row") + " " + (props.currAsteroid.id === item.id ? "selected" : "")} 
                 onClick = {() => setAsteroid(item.id)}
                 >
-                    <td>{item.name}</td>
-                    <td>{Math.round(item.estimated_diameter.feet.estimated_diameter_min) + " Ft"}</td>
-                    <td>{item.close_approach_data[0].close_approach_date}</td>
-                </tr>
+                    <div className = "table-data">{item.name}</div>
+                    <div className = "table-data">{Math.round(item.estimated_diameter.feet.estimated_diameter_min) + " Ft"}</div>
+                    <div className = "table-data">{item.close_approach_data[0].close_approach_date}</div>
+                </div>
             )
         })
 
@@ -34,7 +34,8 @@ function AsteroidList(props) {
         //Get values of original array, not the reference
         let sortedData = props.data.slice(0);
 
-        //Sort contents based on what column was clicked
+        //Sort contents based on what column was clicked.
+        //-1 means b comes before a, 1 means a comes before b.
         if(colName === "name") {
             sortedData = sortedData.sort((a,b) => {
                 if(a.name > b.name) {return -1};
@@ -70,18 +71,14 @@ function AsteroidList(props) {
                     <div className = "sub-header">View nearby comets and asteroids.</div>
                 </div>
                 <div className = "list-container">
-                    <table>
-                        <thead>
-                            <tr className = "table-header">
-                                <th onClick = {() => sortBy("name")}>Name</th>
-                                <th onClick = {() => sortBy("size")}>Size (min)</th>
-                                <th onClick = {() => sortBy("date")}>Passes on</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {props.data ? renderListItems(props.data) : <tr><td className = "loading-text">Loading, please wait...</td></tr>}
-                        </tbody>
-                    </table>
+                    <div className = "table-header-container">
+                        <div className = "table-header-name" onClick = {() => sortBy("name")}>Name</div>
+                        <div className = "table-header-size" onClick = {() => sortBy("size")}>Size (min)</div>
+                        <div className = "table-header-date" onClick = {() => sortBy("date")}>Passes on</div>
+                    </div>
+                    <div className = "table-body-container">
+                        {props.data ? renderListItems(props.data) : <div><div className = "loading-text">Loading, please wait...</div></div>}
+                    </div>
                 </div>
             </div>
         </div>
